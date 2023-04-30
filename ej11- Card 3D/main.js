fc1c1a50bc24a3004a0cb65f8288d590a985c81b
TC <tc@MacBook-Pro-de-TC.local>
2023-04-30 17:33:29 -0300

    terminado ejercicio 3, falta cometarios en 2 y 3

--- ej11- Card 3D/main.js
+++ ej11- Card 3D/main.js
@@ -1,5 +1,25 @@
 "use strict"
+let cardDark = document.querySelector(".card_dark");
+let cardDarkW = cardDark.clientWidth;
+let cardDarkH = cardDark.clientHeight;
 
 function main(){
-    console.log("anda");
+    let clicked = false
+    cardDark.addEventListener('mousemove', (e) => {
+        if(clicked)
+            return;
+        let {layerX, layerY} = e;
+        let rotationX = ( (layerX - cardDarkW / 2) / cardDarkW) * 20;
+        let rotationY = ( (layerY - cardDarkH / 2) / cardDarkH) * 20;
+        cardDark.style.transform =`perspective(650px)
+                                   rotateX(${rotationX}deg)
+                                   rotateY(${rotationY}deg)`;
+    });
+    cardDark.addEventListener('mouseout', (e) => {
+        if (clicked)
+            return
+        cardDark.style.transform = `perspective(650px)
+                                    rotateX(0deg)
+                                    rotateY(0deg)`;
+    });
 }
